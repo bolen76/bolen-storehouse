d64656091550936da7655ff89ad89d0b06dce41e
bolen76 <zhangzhoujun76@163.com>
2019-06-10 19:47:42 +0800

    删除无用的

--- bolen-demo/bin/src/main/webapp/jsp/user.js
+++
@@ -1,4 +0,0 @@
-var a=function(){
-	
-	$("#aa")
-}
--- bolen-demo/bin/src/main/webapp/myJS/array.js
+++
@@ -1,9 +0,0 @@
-var array=new Array();
-array.push("hello");
-array.push(3);
-array.forEach(function(item,index,array){
-	debugger;
-	item+=item;
-	
-});
-alert(array);
