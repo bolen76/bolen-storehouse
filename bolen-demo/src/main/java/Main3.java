f818d253810f0851bb55a6c9f99a590582878935
bolen76 <zhangzhoujun76@163.com>
2020-07-29 22:46:20 +0800

    upload

--- bolen-demo/src/main/java/Main3.java
+++ bolen-demo/src/main/java/Main3.java
@@ -7,36 +7,36 @@ import java.util.Set;
 
 public class Main3 {
 	public static void main(String[] args) {
-		 Scanner in = new Scanner(System.in);
-		 
-		 while(in.hasNextInt()){
-			 
-			 int num=in.nextInt();
-			 List<Integer> arr=new ArrayList<>(num);
-			 for(int i=0;i<num;i++){
-				 arr.add(in.nextInt());
-			 }
-			 List<Integer> sortList = sort(arr);
-			 
-			 for(Integer i:sortList){
-				 
-				 System.out.println(i);
-			 }
-			 
-			 
-		 }
+		Scanner in = new Scanner(System.in);
+
+		while (in.hasNextInt()) {
+
+			int num = in.nextInt();
+			List<Integer> arr = new ArrayList<>(num);
+			for (int i = 0; i < num; i++) {
+				arr.add(in.nextInt());
+			}
+			List<Integer> sortList = sort(arr);
+
+			for (Integer i : sortList) {
+
+				System.out.println(i);
+			}
+
+		}
 	}
-	public static List<Integer> sort(List<Integer> arr){
-		
-		Set set=new HashSet<>();
-		
-		for(int i:arr){
+
+	public static List<Integer> sort(List<Integer> arr) {
+
+		Set set = new HashSet<>();
+
+		for (int i : arr) {
 			set.add(i);
 		}
-		
-		List<Integer> list=new ArrayList<>();
+
+		List<Integer> list = new ArrayList<>();
 		list.addAll(set);
-		
+
 		list.sort(new Comparator<Integer>() {
 
 			@Override
@@ -44,12 +44,11 @@ public class Main3 {
 				// TODO Auto-generated method stub
 				return o1.compareTo(o2);
 			}
-			
+
 		});
-		
+
 		return list;
-		
+
 	}
-	
-	
+
 }
---
+++ bolen-demo/src/main/java/com/bolen/basic/ShutDownHookDemo.java
@@ -0,0 +1,40 @@
+package com.bolen.basic;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ShutDownHookDemo {
+
+	public static void main(String[] args) {
+		ShutDownTask task = new ShutDownTask();
+		Runtime.getRuntime().addShutdownHook(task);
+		/*
+		 * try { System.in.read(); } catch (IOException e1) { // TODO Auto-generated
+		 * catch block e1.printStackTrace(); }
+		 */
+
+		try {
+			Thread.sleep(100000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
+
+class ShutDownTask extends Thread {
+	@Override
+	public void run() {
+
+		try {
+			FileWriter fileWriter = new FileWriter(new File("a.txt"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("hello word!");
+
+	}
+
+}
