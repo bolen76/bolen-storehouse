f0fe264f36a87246bc52ba9d533d29772486d839
bolen76 <zhangzhoujun76@163.com>
2020-07-29 23:28:49 +0800

    upload

--- bolen-demo/src/main/java/A.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class A {
-	public static void main(String[] args) {
-		String valueOf = String.valueOf("a");
-		ArrayList<Object> arrayList = new ArrayList<>();
-	}
-}
