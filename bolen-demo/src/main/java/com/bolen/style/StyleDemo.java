6e4dc249ea6fcd070f0e26113bcd83293b6cf78a
bolen76 <zhangzhoujun76@163.com>
2021-01-29 21:22:26 +0800

    update

---
+++ bolen-demo/src/main/java/com/bolen/style/StyleDemo.java
@@ -0,0 +1,17 @@
+package com.bolen.style;
+
+public class StyleDemo {
+	private String name;
+
+	private final int age = 18;
+
+	public static String weight;
+
+	public static final String LOVE_LANGUAGE = "hello world";
+
+	public static String smethod(String name) {
+
+		return name;
+	}
+
+}
