43e52bd20379092a59bc179ce739c9b4d26fef01
zhangzhoujun <zhangzj212@chinatelecom.cn>
2022-08-19 11:12:56 +0800

    文档整理

--- bolen-demo/src/main/java/com/bolen/string/StringDemo.java
+++ bolen-demo/src/main/java/com/bolen/string/StringDemo.java
@@ -3,22 +3,34 @@ package com.bolen.string;
 import org.junit.Test;
 
 public class StringDemo {
-	
-	@Test
-	public void test01()	{
-		String s1="";
-		String s2="   ";
-		String s3=null;
-		String s4=" hello";
-		System.out.println(s1.isEmpty());
-		System.out.println(s2.isEmpty());
-		
-		
-		System.out.println(s4.isEmpty());
-		System.out.println(s2.trim().isEmpty());
-		System.out.println(s3.isEmpty());
-		
-		StringBuffer sb=new StringBuffer();
-		sb.append("");
-	}
+
+    @Test
+    public void test01() {
+        String s1 = "";
+        String s2 = "   ";
+        String s3 = null;
+        String s4 = " hello";
+        System.out.println(s1.isEmpty());
+        System.out.println(s2.isEmpty());
+
+        System.out.println(s4.isEmpty());
+        System.out.println(s2.trim().isEmpty());
+        System.out.println(s3.isEmpty());
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("");
+    }
+
+    @Test
+    public void test02() {
+        String str = "ab123ab123aab";
+        String replace = str.replace("ab", "xx");
+        System.out.println(str);
+        System.out.println(replace);
+
+        String replaceAll = str.replaceAll("ab", "xx");
+        System.out.println(str);
+        System.out.println(replaceAll);
+
+    }
 }	
