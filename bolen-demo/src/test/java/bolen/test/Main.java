2a661ff355841da5e91ba5d62b78b7f0997687ca
zhangzhoujun <zhangzj212@chinatelecom.cn>
2022-11-21 14:12:53 +0800

    订单工具类包结构调整

--- bolen-demo/src/test/java/bolen/test/Main.java
+++ bolen-demo/src/test/java/bolen/test/Main.java
@@ -1,11 +1,19 @@
 package bolen.test;
 
+import java.math.BigInteger;
+
 public class Main {
-	
-	public static void main(String[] args) {
-		int numericValue = Character.getNumericValue('a');
-		System.out.println(numericValue);
-		int numericValue2 = Character.getNumericValue('1');
-		System.out.println(numericValue2);
-	}
+
+    public static void main(String[] args) {
+        BigInteger b1 = new BigInteger("100");
+        BigInteger b2 = new BigInteger("200");
+        int i = b2.compareTo(b1);
+        System.out.println(i);
+
+        boolean a = Integer.MIN_VALUE - 1 == Integer.MAX_VALUE;
+        System.out.println(a);
+
+        BigInteger min_large_int = new BigInteger("2").shiftLeft(127).negate();
+        BigInteger max_large_int = new BigInteger("2").shiftLeft(127).subtract(new BigInteger("1"));
+    }
 }
